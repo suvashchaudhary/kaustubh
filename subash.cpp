1a34a5211e9f7e90f3d22731b1d12c4e34881965
Subash Chaudhary <suvashchaudhary04@gmail.com>
2024-08-22 06:40:02 +0545

    putting new file in the github

---
+++ subash.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a, l;
+    cout<<"enter length of square:\n";
+    cin>>l;
+    a=l * l;
+    cout<<"area=";
+    cout<<a;
+    return 0;
+}
